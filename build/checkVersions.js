4b2b2e82cfea98cde338aa0a4db70c52ec41e73f
刘明 <1185090651@qq.com>
2021-07-06 21:51:21 +0800

    feat: init

---
+++ build/checkVersions.js
@@ -0,0 +1,50 @@
+const chalk = require('chalk')
+const semver = require('semver')
+const packageConfig = require('../package.json')
+const shell = require('shelljs')
+
+console.log(shell);
+
+function exec (cmd) {
+  return require('child_process').execSync(cmd).toString().trim()
+}
+
+const versionRequirements = [
+  {
+    name: 'node',
+    currentVersion: semver.clean(process.version),
+    versionRequirement: packageConfig.engines.node
+  }
+]
+
+if (shell.which('npm')) {
+  versionRequirements.push({
+    name: 'npm',
+    currentVersion: exec('npm --version'),
+    versionRequirement: packageConfig.engines.npm
+  })
+}
+
+(function () {
+  const warnings = []
+
+  for (let i = 0; i < versionRequirements.length; i++) {
+    const mod = versionRequirements[i]
+
+    if (!semver.satisfies(mod.currentVersion, mod.versionRequirement)) {
+      warnings.push(mod.name + ': ' +
+        chalk.red(mod.currentVersion) + ' should be ' +
+        chalk.green(mod.versionRequirement)
+      )
+    }
+  }
+
+  if (warnings.length) {
+    console.log(chalk.yellow('\nTo use this template, you must update following to modules:\n'))
+    warnings.forEach(warning => {
+        console.log('  ' + warning)
+    })
+    console.log()
+    process.exit(1)
+  }
+})()
